481982e8b45df1e7a74bc3d54449e42a0a633aa8
zapster27 <tilzw27@gmail.com>
2022-03-20 13:08:26 +0530

    first commit

---
+++ build/imu_reader/CMakeFiles/imu_reader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/Documents/diff_drive_tut/devel/lib/imu_reader/imu_reader"
+  "/home/ubuntu/Documents/diff_drive_tut/devel/lib/imu_reader/imu_reader.pdb"
+  "CMakeFiles/imu_reader.dir/src/imu_reader.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imu_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
